a0aa88263172eaee443d01ee1d4b32fa618831bb
mohongen <wuya@17weiju.com>
2020-03-14 19:44:56 +0800

    loading first time

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import ShareReader from "@/components/share-reader"
 export default {
   metaInfo() {
     return {
-      title: "欢迎来到sshPlayer"
+      title: "云音乐系统"
     }
   },
   components: { Layout, MiniPlayer, Playlist, Player, ShareReader }
